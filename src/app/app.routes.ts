5cdd3176fb52a27b9414249259de4233a17ae507
Victor Andres Marimon Mendoza <vmarimon1@udi.edu.co>
2025-02-16 23:42:57 -0500

    feat: :sparkles: Different website paths are added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { UserRegisterComponent } from './user-register/user-register.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { AuthGuard } from './auth/auth-guard';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login', component:UserLoginComponent},
+    {path:'', redirectTo:'login', pathMatch:'full'},
+    {path:'register', component:UserRegisterComponent},
+    {path:'home', component:HomeComponent, canActivate: [AuthGuard]}
+];
